af6d5ee348c9cd3413a85b8bab786cba1b980c1a
Frank-HP-Chang <62094277+Frank-HP-Chang@users.noreply.github.com>
2020-03-26 15:31:08 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -45,8 +45,8 @@ def ml_loop():
         # 3.3. Put the code here to handle the scene information
 
         # 3.4. Send the instruction for this frame to the game process
-        if not ball_served:
-            comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
-            ball_served = True
-        else:
-            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+      #  if not ball_served:
+    #        comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
+   #         ball_served = True
+     #   else:
+           # comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
